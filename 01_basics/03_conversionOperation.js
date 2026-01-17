40af21fca5b2b697dea530504aabe775ebb481f2
Dipti5913 <diptiutale5913@gmail.com>
2026-01-17 07:21:18 +0000

    Add 03_conversionOperation.js under 01_basics folder

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33" 
+//  33
+// let score="33abc" NaN
+// let score = null;
+// let score = undefined
+// let score = true 1 false 0
+
+
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// // conversion from string to number
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); 
+// let isLoggedIn = 1
+
+let isLoggedIn = "true"; // or false, 0, 1, etc.
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+ 
+// 1 = true 0= false 
+// "" = false
+// "Dipti"=true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
