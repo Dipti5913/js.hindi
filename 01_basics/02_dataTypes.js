1ddcd552fbdcf3df18d6ba2635426ab92c781e60
Dipti5913 <diptiutale5913@gmail.com>
2026-01-17 06:28:47 +0000

    Add 02_dataTypes.js under 01_basics folder

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; 
+// treat all js code as newer version
+// alert(3+3) we are using nodejs , not browser
+
+console.log(3+3); 
+// code readability should be high
+console.log("Dipti");
+
+let name = "Dipti"
+let age = 18
+let isLoggedIn = false
+
+// types of datatypes 
+// number = 2 to power 53
+// bigint
+// string=""
+// boolean="true" or "false"
+// null=standalone value(representation of empty value)
+// undefined= not defined value
+// symbol: for find uniqueness
+
+
+
+// object
+
+console.log(typeof "Dipti");
+console.log(typeof undefined)
+console.log(typeof null)
