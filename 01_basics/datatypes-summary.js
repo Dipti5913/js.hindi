739004d20a5fdba838f531a7f811c57ddbdcf9fd
Dipti5913 <diptiutale5913@gmail.com>
2026-01-19 06:16:57 +0000

    datatypessummary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,10 +2,34 @@
 
 // 7 types : string, number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100;
-const scoreValue = 100;
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp= null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34563474975759n
 
 // Reference(Non primitive)
 
 //3 types : Array , Object, Functions
 
+const heros = ["Shaktiman" , "Nagraj", "doga"];
+let myObj = 
+{
+    name:"Dipti" ,
+    age:2,
+}
+
+const myfunction = function()
+{
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
