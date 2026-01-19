74b93af74914dcf7d544b3c420c3e9fd58690d10
Dipti5913 <diptiutale5913@gmail.com>
2026-01-19 05:40:35 +0000

    Add 04_comparison.js u nder 01_basics folder

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+// Primitive (call by value)
+
+// 7 types : string, number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100;
+
+// Reference(Non primitive)
+
+//3 types : Array , Object, Functions
+
